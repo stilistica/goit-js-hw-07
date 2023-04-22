86c924da409623323c74cf8f0225bfa4c29657e6
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-22 17:22:03 +0300

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'; 
+
+const ul = document.querySelector(".gallery");
+const imgMakup = createGalleryMarkup(galleryItems);
+
+
+ul.insertAdjacentHTML('beforeend', imgMakup);
+ul.addEventListener('click', onImgClick);
+
+function createGalleryMarkup(gallery) {
+	return gallery.map(({ preview, original, description }) => {
+		return `
+		<li class="gallery__item">
+		<a class="gallery__link" href="${original}">
+		<img src="${preview}" alt="${description}" data-source="${original}" class="gallery__image" />
+		</a>
+		</li>
+		`;
+	}).join("");
+} 
+
+
+function onImgClick(evt) {
+	evt.preventDefault();
+	if (!evt.target.classList.contains("gallery__image"))	return;
+	const currentImgUrl = evt.target.dataset.source;
+	const instance = basicLightbox.create(`
+		<img src="${currentImgUrl}" />
+		`,
+  {
+			onShow: (instance) => {
+				window.addEventListener('keydown', onEscKeyPress);
+			},
+			onClose: (instance) => {
+				window.removeEventListener('keydown', onEscKeyPress);
+			},
+		}
+	);
+
+	instance.show();
+	
+  function onEscKeyPress(e) {
+    if (e.code !== 'Escape') return;
+    instance.close();
+  }
+}
+
+
 
 console.log(galleryItems);
+
+
+
+
